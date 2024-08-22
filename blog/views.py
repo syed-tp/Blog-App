951d9464ff7df57a46b945af58f2231a6f03f794
syed <syed@testpress.in>
2024-08-22 16:26:34 +0530

    feat: Add form handling, integrate third-party apps, build complex QuerySets

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render
 
+from django.db.models import Count
+
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 
 from django.core.paginator import Paginator, EmptyPage,\
     PageNotAnInteger
 
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 from django.core.mail import send_mail 
 
-def post_list(request):
+from taggit.models import Tag
+
+def post_list(request, tag_slug=None):
+
     posts = Post.published.all()
 
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)  #
+
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
@@ -26,7 +39,7 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'page':page, 'posts': posts})
+                  {'page':page, 'posts': posts, 'tag' :tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -35,9 +48,37 @@ def post_detail(request, year, month, day, post):
                             publish__year=year,
                             publish__month=month,
                             publish__day=day)
+    
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags','-publish')[:4]
+
     return render(request, 
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments' : comments,
+                   'new_comment':new_comment,
+                   'comment_form' : comment_form,
+                   'similar_posts': similar_posts})
 
 from django.views.generic import ListView
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -125,11 +126,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'syed@testpress.in'
-EMAIL_HOST_PASSWORD = 'Syed@4707'
+EMAIL_HOST_PASSWORD = 'tsxv dsvk rxfy pgia'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
