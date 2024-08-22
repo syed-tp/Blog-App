d994942766a30fd56c41ef3093d839b582ba3c14
syed <syed@testpress.in>
2024-08-22 12:16:08 +0530

    feature sharing post via email

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,10 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage,\
     PageNotAnInteger
 
+from .forms import EmailPostForm
+
+from django.core.mail import send_mail 
+
 def post_list(request):
     posts = Post.published.all()
 
@@ -42,3 +46,24 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id =post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'syed@testpree.in', 
+                      [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'syed@testpress.in'
+EMAIL_HOST_PASSWORD = 'Syed@4707'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL= 'syed@testpress.in'
