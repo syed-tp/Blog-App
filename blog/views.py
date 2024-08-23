5229e008dcab77e077ffec05d9afb1c160f0a957
syed <syed@testpress.in>
2024-08-23 15:30:03 +0530

    feat: searching with trigram similarity with postgreSql

--- blog/views.py
+++ blog/views.py
@@ -8,11 +8,15 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,\
     PageNotAnInteger
 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 from django.core.mail import send_mail 
 
 from taggit.models import Tag
+
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
+from django.contrib.postgres.search import TrigramSimilarity
 
 def post_list(request, tag_slug=None):
 
@@ -108,3 +112,28 @@ def post_share(request, post_id):
 
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+
+            search_vector = SearchVector('title', weight='A') + \
+                SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+                # search=search_vector,
+                # rank = SearchRank('search_vector', 'search_query'),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+    
+    return render(request, 'blog/post/search.html', {
+        'form': form,
+        'query': query,
+        'results': results
+    })
